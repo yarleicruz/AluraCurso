161c071bf14e714dbc2adb7b23d2def090f9af9a
Yarlei Cruz <yarleicruz011@gmail.com>
2024-03-13 16:02:09 -0300

    new

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,9 +1,22 @@
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
-function tocaSomClap() {
-    document.querySelector('#som_tecla_clap').play();
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++){
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function(){
+        tocaSom(idAudio)
+    }
+
+    tecla.onkeydown = function (){
+        tecla.classList.add('ativa')
+    }
+
 }
-document.querySelector('.tecla_clap');
